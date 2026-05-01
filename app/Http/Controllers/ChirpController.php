5955af712ae18cc3d9c7ff139adde01137956871
liliie09 <lili.240170120@mhs.unimal.ac.id>
2026-05-01 19:35:52 +0700

    selesai latihan part 2 sesuai ppt

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -7,7 +7,26 @@ use Illuminate\Http\Request;
 class ChirpController extends Controller
 {
     public function index()
-    {
-        return view('welcome');
-    }
+{
+    $chirps = [
+        [
+            'author' => 'Jane Doe',
+            'message' => 'Just deployed my first Laravel app! 🚀',
+            'time' => '5 minutes ago'
+        ],
+        [
+            'author' => 'John Smith',
+            'message' => 'Laravel makes web development fun again!',
+            'time' => '1 hour ago'
+        ],
+        [
+            'author' => 'Alice Johnson',
+            'message' => 'Working on something cool with Chirper...',
+            'time' => '3 hours ago'
+        ]
+    ];
+
+    // Mengirim data array $chirps ke view 'welcome' dengan nama variabel 'chirps'
+    return view('welcome', ['chirps' => $chirps]);
+}
 }
